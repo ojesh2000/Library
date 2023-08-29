355e420fb1cc6ea4532a5b161a4b719fa4494a48
ojesh2000 <ojeshparashar@gmail.com>
2023-08-29 12:49:05 +0530

    Added Service Layer

--- src/main/java/com/library/LibraryProject/Controller/BookController.java
+++ src/main/java/com/library/LibraryProject/Controller/BookController.java
@@ -6,6 +6,7 @@ import com.library.LibraryProject.Repository.AuthorRepository;
 import com.library.LibraryProject.Repository.BookRepository;
 import com.library.LibraryProject.Repository.SearchAuthorRepository;
 import com.library.LibraryProject.Repository.SearchRepositorty;
+import com.library.LibraryProject.Service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,13 +20,7 @@ public class BookController {
     AuthorRepository arepo;
 
     @Autowired
-    SearchAuthorRepository sarepo;
-
-    @Autowired
-    BookRepository repo;
-
-    @Autowired
-    SearchRepositorty srepo;
+    private BookService service;
     @RequestMapping("/")
     public String getRoot(){
         return "Hello World";
@@ -33,48 +28,40 @@ public class BookController {
 
     @GetMapping("/allbooks")
     public List<Book> getAllBooks(){
-        return repo.findAll();
+        return service.getAllBooks();
     }
 
     @GetMapping("allauthors")
-    public List<Author> getAllAuthors(){return arepo.findAll();}
+    public List<Author> getAllAuthors(){return service.getAllAuthors();}
 
     @GetMapping("/books/{text}")
     public List<Book> search(@PathVariable String text){
-        return srepo.findByText(text);
+        return service.search(text);
     }
 
     @GetMapping("/books/{text}/{num}")
     public List<Book> searchByTextAndNum(@PathVariable String text , @PathVariable long num){
-        return srepo.findByTextGreater(text , num);
+        return service.searchByTextAndNum(text , num);
     }
 
     @PostMapping("/save-book")
     public void saveBook(@RequestBody Book book){
-        if(book.getId() == null || book.getGenre() == null)return ;
-        repo.save(book);
+        service.saveBook(book);
     }
 
     @PostMapping("/save-author")
     public void saveAuthor(@RequestBody Author auth)    {
-        System.out.println(auth.getAddress());
-        arepo.insert(auth);
+        service.saveAuthor(auth);
     }
 
     @GetMapping("/find-books/{authName}")
     public List<Book> findBooksBasedOnAuth(@PathVariable String authName){
-        List<Integer> authIds = sarepo.findAuthors(authName);
-        List<Book> booksByAuth = new ArrayList<>();
-        for(Integer id : authIds){
-            List<Book> reqd = srepo.findByAuthId(id);
-            booksByAuth.addAll(reqd);
-        }
-        return booksByAuth;
+        return service.findBooksBasedOnAuth(authName);
     }
 
     @GetMapping("/find-auth/{likeAuth}")
     public List<Author> findAuthorsRegex(@PathVariable String likeAuth){
-        return sarepo.findAuthorsByRegex(likeAuth);
+        return service.findAuthorsRegex(likeAuth);
     }
 
 }
